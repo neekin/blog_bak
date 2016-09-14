ecff5845a69fd31d957efc297ff22df032595a98
NeeKin <njgzs360@gmail.com>
2016-09-15 01:02:11 +0800

    fix deploy

--- gulpfile.js
+++ gulpfile.js
@@ -120,7 +120,7 @@ gulp.task("images", function () {
 
 
 //用于生产环境
-gulp.task("deploy", ["clean", "dev", "delDeploymentDir"], function () {
+gulp.task("deploy", ["delDeploymentDir"], function () {
     return sequence(["imgmin"], ["cssmin"], ["jsmin"], ["htmlmin"]);
 });
 gulp.task("delDeploymentDir", function (cb) {
